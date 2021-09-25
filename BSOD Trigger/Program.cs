02668a3e6e9eba99b63953bae29f5a88a301e830
DevPieter <superpieter88@gmail.com>
2021-09-25 18:12:43 +0200

    Initial commit

---
+++ BSOD Trigger/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace BSOD_Trigger
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string path = Path.GetTempPath() + "trigger_bsod.txt";
+                if (File.Exists(path))
+                    File.Delete(path);
+
+                Console.Title = "Don't Take Damage | IMPORTAN Don't close this window!";
+                Console.WindowWidth = 131;
+                Console.WindowHeight = 14;
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("▓█████▄  ▒█████   ███▄    █ ▄▄▄█████▓   ▄▄▄█████▓ ▄▄▄       ██ ▄█▀▓█████    ▓█████▄  ▄▄▄       ███▄ ▄███▓ ▄▄▄        ▄████ ▓█████ \n▒██▀ ██▌▒██▒  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒   ▓  ██▒ ▓▒▒████▄     ██▄█▒ ▓█   ▀    ▒██▀ ██▌▒████▄    ▓██▒▀█▀ ██▒▒████▄     ██▒ ▀█▒▓█   ▀ \n░██   █▌▒██░  ██▒▓██  ▀█ ██▒▒ ▓██░ ▒░   ▒ ▓██░ ▒░▒██  ▀█▄  ▓███▄░ ▒███      ░██   █▌▒██  ▀█▄  ▓██    ▓██░▒██  ▀█▄  ▒██░▄▄▄░▒███   \n░▓█▄   ▌▒██   ██░▓██▒  ▐▌██▒░ ▓██▓ ░    ░ ▓██▓ ░ ░██▄▄▄▄██ ▓██ █▄ ▒▓█  ▄    ░▓█▄   ▌░██▄▄▄▄██ ▒██    ▒██ ░██▄▄▄▄██ ░▓█  ██▓▒▓█  ▄ \n░▒████▓ ░ ████▓▒░▒██░   ▓██░  ▒██▒ ░      ▒██▒ ░  ▓█   ▓██▒▒██▒ █▄░▒████▒   ░▒████▓  ▓█   ▓██▒▒██▒   ░██▒ ▓█   ▓██▒░▒▓███▀▒░▒████▒\n ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒   ▒ ░░        ▒ ░░    ▒▒   ▓▒█░▒ ▒▒ ▓▒░░ ▒░ ░    ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒░   ░  ░ ▒▒   ▓▒█░ ░▒   ▒ ░░ ▒░ ░\n ░ ▒  ▒   ░ ▒ ▒░ ░ ░░   ░ ▒░    ░           ░      ▒   ▒▒ ░░ ░▒ ▒░ ░ ░  ░    ░ ▒  ▒   ▒   ▒▒ ░░  ░      ░  ▒   ▒▒ ░  ░   ░  ░ ░  ░\n ░ ░  ░ ░ ░ ░ ▒     ░   ░ ░   ░           ░        ░   ▒   ░ ░░ ░    ░       ░ ░  ░   ░   ▒   ░      ░     ░   ▒   ░ ░   ░    ░   \n   ░        ░ ░           ░                            ░  ░░  ░      ░  ░      ░          ░  ░       ░         ░  ░      ░    ░  ░\n ░                                                                           ░                                                    \n");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Don't Take Damage By DevPieter");
+                Console.WriteLine($"Waiting for the trigger file... ({path})");
+
+                while (true)
+                    if (File.Exists(path))
+                        foreach (Process process in Process.GetProcessesByName("svchost"))
+                            process.Kill();
+            }
+            catch { }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/K3K05621Y)
+<br>
+# BSOD Trigger (for [Don't Take Damage](https://github.com/DevPieter/Dont-Take-Damage))
+
+<p align="center">
+<img src="https://img.shields.io/static/v1?label=Made%20With&message=%E2%9D%A4&color=red"/>
+<img src="https://img.shields.io/static/v1?label=By&message=DevPieter&color=blueviolet"/>
+</p>
+
+## WARNING: DevPieter isn't responsible for any damage or data loss!
+<br>
+
+# About
+
+This application triggers the blue screen for the mod [Don't Take Damage](https://github.com/DevPieter/Dont-Take-Damage).
+
+When it finds a file named 'trigger_bsod.txt' in the temporary directory (%temp%), it will trigger the blue screen.
+
+It triggers a blue screen by killing the process 'svchost.exe'
+
+<br><br>
+
+<p align="center"> 
+<img src="https://github.com/DevPieter/Dont-Take-Damage/raw/main/src/main/resources/assets/donttakedamage/icon.png" alt="Inco"/>
+</p>
